cd5ceafb0eb70712d921678c77ca99af4490ce14
Atharva7626 <137627556+Atharva7626@users.noreply.github.com>
2024-04-14 11:38:24 +0530

    Update a3.php

--- a3.php
+++ a3.php
@@ -1,5 +1,29 @@
 <?php
-
-
-
+  $str =<<<XML
+  <?xml version="1.0" encoding="UTF-8"?>
+  <BookInfo>
+       <book>
+         <bookNO>1</bookNO>
+         <bookName>Java</bookName>
+         <authorname>Balguru swami</authorname>
+         <price>10</price>
+         <year>2006</year> 
+      </book>
+      <book>
+         <bookNO>2</bookNO>
+         <bookName>C</bookName>
+         <authorname>Dennnis Ritchie</authorname>
+         <price>500</price>
+         <year>1971</year> 
+      </book>     
+  </BookInfo>
+  XML;
+  $file="BookInfo.xml";
+  $fp=fopen($file,'w');
+  fwrite($fp,$str);
+  fclose($fp);
+  
+  echo"<br> Created xml file successfully ".$file."<br>";
+  echo"<br> File contents are : <br>";
+  readfile($file);
 ?>
